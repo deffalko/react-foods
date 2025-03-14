3f3473615c0ecbad8eaa3b67149339d4d8109a74
артур <gumarovartur059@gmail.com>
2025-03-14 21:39:28 +0300

    API & page Home catalog

---
+++ src/components/CategoryItem.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+function CategoryItem(props) {
+  const { idCategory, strCategory, strCategoryThumb, strCategoryDescription } =
+    props;
+  return (
+    <div className="card">
+      <div className="card-image">
+        <img src={strCategoryThumb} alt={strCategory} />
+      </div>
+      <div className="card-content">
+        <p>{strCategoryDescription.slice(0, 60)}...</p>
+        <span className="card-title">{strCategory}</span>
+      </div>
+      <div className="card-action">
+        <Link to={`/category/${idCategory}`} className="btn">
+          Watch categoty
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export { CategoryItem };
